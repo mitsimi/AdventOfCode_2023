aeedb501ca1c422a504917e6ee6ce56a9e2ba513
Noah Füreder <noah.fuereder@gmail.com>
2024-06-23 14:18:53 +0200

    Day 5 - Part 1/2

--- Day 5/go.mod
+++ Day 5/go.mod
@@ -1,3 +1,3 @@
 module Day_5
 
-go 1.21
+go 1.22
--- Day 5/seeds.go
+++ Day 5/seeds.go
@@ -2,140 +2,69 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"regexp"
 	"slices"
 	"strings"
-	"time"
 	"util"
 )
 
-func main() {
-	sc := util.NewScanner("input")
-	sc.Scan()
-	// parse all numbers of the first line
-	seedIds := parseNums.FindAllString(sc.Text(), -1)
-	log.Println("parsing seeds")
-	for _, id := range seedIds {
-		seeds = append(seeds, util.Atoi(id))
-	}
-
-	// parse map content
-	log.Println("started parsing mappings")
-	for sc.Scan() {
-		line := sc.Text()
-		if line == "" {
-			continue
-		}
-
-		if slices.Contains(keyword, line) {
-			key := line
-			log.Printf("parse mappings for %s \n", key)
-			for sc.Scan() {
-				line := sc.Text()
-				log.Printf("parsing line: %s\n", line)
-				if line == "" {
-					break
-				}
-				parseMap(key, line)
-			}
-		}
-	}
-	log.Println("finished parsing mappings")
-
-	lowest := findLowestLocation()
-	fmt.Println(lowest)
+type Mapping struct {
+	Dest int
+	Src  int
+	Len  int
 }
 
 var parseNums = regexp.MustCompile("[0-9]+")
+var seeds []int
+var ranges []Mapping
 
-var keyword = []string{
-	"seed-to-soil map:",
-	"soil-to-fertilizer map:",
-	"fertilizer-to-water map:",
-	"water-to-light map:",
-	"light-to-temperature map:",
-	"temperature-to-humidity map:",
-	"humidity-to-location map:",
-}
-
-var keywordToMap = map[string]map[int]int{
-	"seed-to-soil map:":            seedToSoil,
-	"soil-to-fertilizer map:":      soilToFertilizer,
-	"fertilizer-to-water map:":     fertilizerToWater,
-	"water-to-light map:":          waterToLight,
-	"light-to-temperature map:":    lightToTemperature,
-	"temperature-to-humidity map:": temperatureToHumidity,
-	"humidity-to-location map:":    humidityToLocation,
-}
-
-var seeds = []int{}
-
-var seedToSoil = make(map[int]int)
-var soilToFertilizer = make(map[int]int)
-var fertilizerToWater = make(map[int]int)
-var waterToLight = make(map[int]int)
-var lightToTemperature = make(map[int]int)
-var temperatureToHumidity = make(map[int]int)
-var humidityToLocation = make(map[int]int)
-
-func parseMap(keyword, line string) {
-	fields := strings.Fields(line) // <dest range start> <source range start> <length>
-	dest := util.Atoi(fields[0])
-	src := util.Atoi(fields[1])
-	length := util.Atoi(fields[2])
-
-	convMap := keywordToMap[keyword]
-	log.Println("create mappings")
-	t := time.Now()
-	for i := 0; i < length; i++ {
-		convMap[src+i] = dest + i
+func main() {
+	file, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
 	}
-	log.Printf("finished mappings within %d ms\n", time.Since(t).Milliseconds())
-}
+	content := strings.Split(string(file), "\n\n")
 
-func findLowestLocation() int {
-	var locs []int
-	log.Println("looking for lowest location")
-	for _, seed := range seeds {
+	nums := util.Atoi_Arr(parseNums.FindAllString(strings.Join(content[:1], ""), -1))
+	for i := 0; i < len(nums); i += 2 {
+		for offset := range nums[i+1] {
+			seeds = append(seeds, nums[i]+offset)
+		}
+	}
+	fmt.Println("parsed seeds")
 
-		soil := seedToSoil[seed]
-		if soil == 0 {
-			soil = seed
+	for i, block := range content[1:] {
+		for _, line := range strings.Split(block, "\n")[1:] {
+			if line == "" {
+				break
+			}
+
+			nums := util.Atoi_Arr(parseNums.FindAllString(line, -1))
+			ranges = append(ranges, Mapping{Dest: nums[0], Src: nums[1], Len: nums[2]})
 		}
 
-		fert := soilToFertilizer[soil]
-		if fert == 0 {
-			fert = soil
-		}
+		new := make([]int, len(seeds))
+		for i, seed := range seeds {
+			found := false
+			for _, r := range ranges {
+				if r.Src <= seed && seed < (r.Src+r.Len) {
+					new[i] = seed - r.Src + r.Dest
+					found = true
+					break
+				}
+			}
 
-		water := fertilizerToWater[fert]
-		if water == 0 {
-			water = fert
-		}
+			if !found {
+				new[i] = seed
+			}
 
-		light := waterToLight[water]
-		if light == 0 {
-			light = water
 		}
+		seeds = new
+		ranges = []Range{}
 
-		temp := lightToTemperature[light]
-		if temp == 0 {
-			temp = light
-		}
-
-		hum := temperatureToHumidity[temp]
-		if hum == 0 {
-			hum = temp
-		}
-
-		loc := humidityToLocation[hum]
-		if loc == 0 {
-			loc = hum
-		}
-
-		locs = append(locs, loc)
+		fmt.Printf("Blocks done: %d / %d \n", i+1, len(content[1:]))
 	}
 
-	return slices.Min(locs)
+	util.Sout(slices.Min(seeds))
 }
