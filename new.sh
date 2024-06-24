baf8aa576b32c719180fb294583700e662b128b9
Noah Füreder <noah.fuereder@gmail.com>
2024-06-24 22:00:39 +0200

    extended script with test

--- new.sh
+++ new.sh
@@ -8,16 +8,60 @@ mkdir $dir
 
 cd $dir
 
+curl --cookie "session=$AOC_COOKIE" https://adventofcode.com/2023/day/$number/input > input
 touch main.go
 echo "package main" > main.go
 echo "
 func main() {
+    file := "input"
+    fmt.Println("Part 1:", part1(file))
+    fmt.Println("Part 2:", part2(file))
+}
 
-}" >> main.go
+func part1(input string) int {
+	panic("not implemented")
+}
+
+func part2(input string) int {
+	panic("not implemented")
+}
+" >> main.go
+
 touch example
-curl --cookie "session=$AOC_COOKIE" https://adventofcode.com/2023/day/$number/input > input
-go mod init $dir
+touch main_test.go
+echo "package main" > main_test.go
+echo "
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
+func TestMain(t *testing.T) {
+	tests := []struct {
+		method   func(string) int
+		expected int
+		input    string
+	}{
+		{
+			method:   part1,
+			expected: 999,
+			input:    "example",
+		},
+		//{
+		//	method:   part1,
+		//	expected: 999,
+		//	input:    "example",
+		//},
+	}
+
+	for _, test := range tests {
+		t.Run(test.method, func(t *testing.T) {
+			assert.Equal(t, test.expected, test.method(test.input))
+		})
+	}
+}
+" >> main_test.go
+
+go mod init $dir
 cd ..
 go work use $dir
-
